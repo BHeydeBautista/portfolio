7ba1bb45e2e7efb032d185b3993173c04d0500ec
Bautista Heyde <bautistaheyde@hotmail.com>
2026-01-20 12:13:20 -0300

    info: sume el login/register

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,25 +12,27 @@ import {
   SiJsonwebtokens,
   SiSwagger,
   SiMinutemailer,
-} from 'react-icons/si'
+  SiPrisma,
+  SiFramer,
+} from "react-icons/si";
 
 export const projects = [
   {
-    title: 'Servicios Libre — Frontend',
+    title: "Servicios Libre — Frontend",
     description:
-      'Plataforma moderna construida con Next.js 15 y TypeScript que conecta clientes con trabajadores mediante chat en tiempo real y gestión de contratos. Comunicación fluida, experiencia optimizada y despliegue continuo en Vercel.',
-    image: '/projects/serviciolibrefront.jpg',
-    stackName: 'Next.js + TailwindCSS',
+      "Plataforma moderna construida con Next.js 15 y TypeScript que conecta clientes con trabajadores mediante chat en tiempo real y gestión de contratos. Comunicación fluida, experiencia optimizada y despliegue continuo en Vercel.",
+    image: "/projects/serviciolibrefront.jpg",
+    stackName: "Next.js + TailwindCSS",
     stack: [SiNextdotjs, SiReact, SiTypescript, SiTailwindcss, SiVercel],
-    link: 'https://serviciolibre.vercel.app',
-    repo: 'https://github.com/Servicios-Libre/frontend',
+    link: "https://serviciolibre.vercel.app",
+    repo: "https://github.com/Servicios-Libre/frontend",
   },
   {
-    title: 'Servicios Libre — Backend API',
+    title: "Servicios Libre — Backend API",
     description:
-      'API robusta desarrollada con NestJS y PostgreSQL, orientada a ofrecer una arquitectura escalable y segura. Integra autentificación, sockets, gestión de archivos, documentación Swagger y comunicación vía email.',
-    image: '/projects/serviciolibreback.jpg',
-    stackName: 'NestJS + PostgreSQL',
+      "API robusta desarrollada con NestJS y PostgreSQL, orientada a ofrecer una arquitectura escalable y segura. Integra autentificación, sockets, gestión de archivos, documentación Swagger y comunicación vía email.",
+    image: "/projects/serviciolibreback.jpg",
+    stackName: "NestJS + PostgreSQL",
     stack: [
       SiNestjs,
       SiTypescript,
@@ -42,25 +44,25 @@ export const projects = [
       SiSwagger,
       SiMinutemailer,
     ],
-    link: 'https://back-servicio-libre.onrender.com/',
-    repo: 'https://github.com/Servicios-Libre/backend',
+    link: "https://back-servicio-libre.onrender.com/",
+    repo: "https://github.com/Servicios-Libre/backend",
   },
   {
-    title: 'Moro Abogados - FrontEnd',
+    title: "Moro Abogados - FrontEnd",
     description:
-      'Diseño moderno para actualizar el sitio del estudio jurídico Moro Abogados. Construido con Next.js 16 y TypeScript. Incluye una estructura clara, layout responsivo y estilos refinados. Despliegue próximo en Vercel.',
-    image: '/projects/moroabogados.jpg',
-    stackName: 'Next.js + TailwindCSS',
+      "Diseño moderno para actualizar el sitio del estudio jurídico Moro Abogados. Construido con Next.js 16 y TypeScript. Incluye una estructura clara, layout responsivo y estilos refinados. Despliegue próximo en Vercel.",
+    image: "/projects/moroabogados.jpg",
+    stackName: "Next.js + TailwindCSS",
     stack: [SiNextdotjs, SiReact, SiTypescript, SiTailwindcss, SiVercel],
-    link: 'https://www.moroabogados.com.ar/',
-    repo: 'https://github.com/BHeydeBautista/Moro-Abogados',
+    link: "https://www.moroabogados.com.ar/",
+    repo: "https://github.com/BHeydeBautista/Moro-Abogados",
   },
   {
-    title: 'Moro Abogados — Backend',
+    title: "Moro Abogados — Backend",
     description:
-      'Diseño moderno para actualizar el sitio del estudio jurídico Moro Abogados. Construido con Next.js 15 y TypeScript. Incluye una estructura clara, layout responsivo y estilos refinados. Despliegue próximo en Vercel.',
-    image: '/projects/moroabogados.jpg',
-    stackName: 'NestJs + TypeScript',
+      "Diseño moderno para actualizar el sitio del estudio jurídico Moro Abogados. Construido con Next.js 15 y TypeScript. Incluye una estructura clara, layout responsivo y estilos refinados. Despliegue próximo en Vercel.",
+    image: "/projects/moroabogados.jpg",
+    stackName: "NestJs + TypeScript",
     stack: [
       SiNestjs,
       SiTypescript,
@@ -69,15 +71,15 @@ export const projects = [
       SiSocketdotio,
       SiCloudinary,
     ],
-    link: 'https://moroabogados-backend-production.up.railway.app/',
-    repo: 'https://github.com/BHeydeBautista/MoroAbogados-Backend',
+    link: "https://moroabogados-backend-production.up.railway.app/",
+    repo: "https://github.com/BHeydeBautista/MoroAbogados-Backend",
   },
   {
-    title: 'Ecommerce M4 Proyecto Integrador — Backend API',
+    title: "Ecommerce M4 Proyecto Integrador — Backend API",
     description:
-      'API robusta desarrollada con NestJS y PostgreSQL, orientada a ofrecer una arquitectura escalable y segura. Integra autentificación, sockets, gestión de archivos, documentación Swagger y comunicación vía email.',
-    image: '/projects/serviciolibreback.jpg',
-    stackName: 'NestJS + PostgreSQL',
+      "API robusta desarrollada con NestJS y PostgreSQL, orientada a ofrecer una arquitectura escalable y segura. Integra autentificación, sockets, gestión de archivos, documentación Swagger y comunicación vía email.",
+    image: "/projects/serviciolibreback.jpg",
+    stackName: "NestJS + PostgreSQL",
     stack: [
       SiNestjs,
       SiTypescript,
@@ -89,7 +91,18 @@ export const projects = [
       SiSwagger,
       SiMinutemailer,
     ],
-    link: '',
-    repo: 'https://github.com/BHeydeBautista/Modulo-4-Henry-PI',
-  }
-]
+    link: "",
+    repo: "https://github.com/BHeydeBautista/Modulo-4-Henry-PI",
+  },
+  {
+    title:
+      "Base reutilizable de Inicio de sesión y creación de cuenta - FrontEnd",
+    description:
+      "API para futuros proyectos, pudiendo importar los componentes ya desarrollados y adaptarlos según la necesidad, ahorrando tiempo y evitando repetir pruebas y configuraciones.",
+    image: "/projects/register-front.png",
+    stackName: "NextJS + TailwindCSS",
+    stack: [SiNextdotjs, SiReact, SiTypescript, SiTailwindcss, SiVercel, SiPrisma, SiFramer],
+    link: "",
+    repo: "https://github.com/BHeydeBautista/Login-And-Register-FrontEnd",
+  },
+];
